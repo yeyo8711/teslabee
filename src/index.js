4b21c89db05a10dd776c3210425810dc04e57371
Paulo <pauloxti@gmail.com>
2023-02-24 18:15:56 +0100

    añadido funciones web3

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Web3Provider } from "@ethersproject/providers";
+import Web3 from 'web3'
 import { Web3ReactProvider } from "@web3-react/core";
 
 
 function getLibrary(provider) {
-  console.log("getting library")
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 12000
-  return library
+  return new Web3(provider)
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
